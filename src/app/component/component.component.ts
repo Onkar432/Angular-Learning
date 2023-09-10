59043b1e12af83111a8ab701fb0bbcf92c9b25ba
Onkar-Bynry <onkar.pathak@bynry.io>
2023-09-10 18:53:18 +0530

    All about events

---
+++ src/app/component/component.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component',
+  templateUrl: './component.component.html',
+  styleUrls: ['./component.component.css']
+})
+export class ComponentComponent {
+  getname(Name: string) {
+    console.log(Name)
+  }
+
+}
